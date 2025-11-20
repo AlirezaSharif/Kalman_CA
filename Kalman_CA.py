52229285f20f12c3d80dfb463eb24bce5282a2d2
AlirezaSharif <Alirezasharifzadeh@yahoo.com>
2025-11-20 17:22:55 +1300

    first implementation with a working example

---
+++ Kalman_CA.py
@@ -0,0 +1,167 @@
+from roukf import ROUKF, SigmaDistribution
+from opencor_helper import SimulationHelper
+import numpy as np
+import matplotlib.pyplot as plt
+import yaml
+
+
+file_path = 'Mohammad.yaml'
+
+
+with open(file_path, 'r') as file:
+        # Load the YAML data into a Python dictionary
+        config = yaml.safe_load(file)
+
+
+
+
+def forward_operator(x: np.ndarray, n_states: int, theta: np.ndarray, n_parameters: int) -> int:
+    """
+    Forward operator A(x, θ) that propagates the state
+    Args:
+        x: numpy array - State vector (modified in-place)
+        n_states: int - Number of states
+        theta: numpy array - Parameter vector (modified in-place)
+        n_parameters: int - Number of parameters
+    Returns:
+        int: Status (1 for success, 0 for failure)
+    """
+    try: 
+       
+        y.set_param_vals(x_0, x[0:n_states])
+        y.set_param_vals(parameters_to_estimate, theta[0:n_parameters])
+        y.run()
+        x[0:n_states] = y.get_results(x_1)[:,-1]
+        y.reset_and_clear()
+
+        return 1
+    
+    except Exception as e:
+        print(f"Error in forward operator: {e}")
+        return 0
+
+def observation_operator(x: np.ndarray, n_states: int, z: np.ndarray, n_observations: int) -> None:
+    """
+    Observation operator H(x) that maps state to observations
+    Args:
+        x: numpy array - State vector (modified in-place) I don't know 
+        n_states: int - Number of states
+        z: numpy array - Observation vector (modified in-place)
+        n_observations: int - Number of observations
+    """
+    try:
+        
+       
+        z[0:n_observations] = x[mapping_indices]
+      
+
+    except Exception as e:
+        print(f"Error in observation operator: {e}")
+
+def data_generator(numSteps):
+    timeSeries = np.zeros((1, numSteps))
+    y = SimulationHelper(f"{config['File_name']}.cellml", 0.00001, 0.001 * num_steps, maximumNumberofSteps=1000, pre_time=0)
+    x_1 = [i  for i in y.simulation.results().states().keys()]
+    parameters_to_estimate = [config['Model_name']  + i for i in config['parameters_to_estimate']]
+    y.set_param_vals(parameters_to_estimate, config['true_parameters'])
+    y.run()
+    x =  y.get_results(x_1)
+    variables_to_observe = [config['Model_name']  + i for i in config['observations']] 
+    mapping_indices  = [x_1.index(item) for item in variables_to_observe]
+    
+    timeSeries = x[mapping_indices, :]
+    # breakpoint()
+    
+    y.reset_and_clear()
+    y.close_simulation()
+
+
+    return timeSeries
+
+num_steps = config['num_steps']
+observations = data_generator(num_steps)
+
+
+
+y = SimulationHelper(f"{config['File_name']}.cellml", 0.00001, 0.00001, maximumNumberofSteps=1000, pre_time=0)
+x_0 = [i  for i in y.simulation.results().states().keys()]
+x_1 = [i for i in y.simulation.results().states().keys()]
+
+parameters_to_estimate = [config['Model_name']  + i for i in config['parameters_to_estimate']]
+variables_to_observe = [config['Model_name']  + i for i in config['observations']] 
+mapping_indices  = [x_1.index(item) for item in variables_to_observe]
+
+# Example dimensions
+n_observations = config['n_observations']  # Number of observations
+n_states = len(y.simulation.results().states().keys())    # Number of states
+n_parameters = config['n_parameters']    # Number of parameters to estimate
+
+# Initialize initialGuess and parametersUncertainties
+parameters_uncertainty = np.array(config['parameters_uncertainty'], dtype=np.float64)
+
+# initial_guess = np.array(config['initial_state'], dtype=np.float64)
+initial_guess = np.zeros((n_states), dtype=np.float64)
+
+for i, state_name in  enumerate(x_0):
+    initial_guess[i] = y.data.states()[state_name]
+
+# initial_guess = np.full(n_states, config['initial_state'], dtype=np.float64)
+# initial_parameters = np.full(n_parameters, 5, dtype=np.float64)
+initial_parameters = np.array(config['initial_parameters'], dtype=np.float64)
+
+# Create uncertainty arrays
+states_uncertainty = np.array(config['states_uncertainty'], dtype=np.float64)
+
+
+# Initialize ROUKF
+kalman_filter = ROUKF(
+    n_observations,
+    n_states,
+    n_parameters,
+    states_uncertainty,
+    parameters_uncertainty,
+    SigmaDistribution.SIMPLEX
+)
+
+initial_state = np.ascontiguousarray(initial_guess)
+initial_parameter = np.ascontiguousarray(initial_parameters)
+
+
+# Set initial condition
+kalman_filter.setState(initial_state)
+kalman_filter.setParameters(initial_parameter)
+observe = np.zeros((n_observations), dtype=np.float64)
+
+
+for i in range(num_steps):
+    # Execute a step of the Kalman filter
+    observe[0:n_observations] = observations[:, i+1]
+
+    error = kalman_filter.executeStep(
+        observe,
+        forward_operator, 
+        observation_operator
+    )
+    
+    state_estimate = kalman_filter.getState()
+    params = kalman_filter.getParameters(n_parameters)
+
+
+    print("###########################")
+
+    print(f"Step_{i}: Error: {error:.6f}, Parameter estimate: {params}")  
+    
+
+
+
+# Reset the Kalman filter
+kalman_filter.reset(
+    n_observations,
+    n_states,
+    n_parameters,
+    states_uncertainty,
+    parameters_uncertainty,
+    SigmaDistribution.SIMPLEX
+)
+print("Kalman filter has been reset.")
+
---
+++ opencor_helper.py
@@ -0,0 +1,226 @@
+import opencor as oc
+import numpy as np
+# from func_timeout import func_timeout, FunctionTimedOut
+
+class SimulationHelper():
+    def __init__(self, cellml_path, dt,
+                 sim_time, maximumNumberofSteps=100000,
+                 maximum_step=0.001, pre_time=0.0):
+        self.cellml_path = cellml_path  # path to cellml file
+        self.dt = dt  # time step
+        self.stop_time = pre_time + sim_time  # full time of simulation
+        self.pre_steps = int(pre_time/dt)  # number of steps to do before storing data (used to reach steady state)
+        self.n_steps = int(sim_time/dt)  # number of steps for storing data
+        self.simulation = oc.open_simulation(cellml_path)
+        if not self.simulation.valid():
+            print(f'simulation object opened from {cellml_path} is not valid, exiting')
+            exit()
+        self.data = self.simulation.data()
+        self.data.set_ode_solver_property('MaximumNumberOfSteps', maximumNumberofSteps)
+        self.data.set_ode_solver_property('MaximumStep', maximum_step)
+        self.data.set_point_interval(dt)  # time interval for data storage
+        self.data.set_starting_point(0)
+        self.data.set_ending_point(self.stop_time)
+        self.tSim = np.linspace(pre_time, self.stop_time, self.n_steps + 1) # time values for stored part of simulation
+
+    def run(self):
+        try:
+            self.simulation.run()
+        # except FunctionTimedOut:
+        #     print("openCOR timed out")
+        #     print('restarting simulation object')
+        #     self.simulation.reset()
+        #     self.simulation.clear_results()
+        #     return False
+        except RuntimeError:
+            print("Failed to converge")
+            print('restarting simulation object')
+            self.simulation.reset()
+            self.simulation.clear_results()
+            return False
+
+        return True
+
+    def reset_and_clear(self):
+        self.simulation.reset()
+        self.simulation.clear_results()
+    def reset_states(self):
+        self.simulation.reset(False)
+        
+    def get_all_variable_names(self):
+        # get all states, algebraics and constants
+        variable_names = list(self.simulation.results().states().keys()) + \
+                                list(self.simulation.results().algebraic().keys()) + \
+                                     list(self.data.constants().keys())
+        return variable_names
+    
+    def get_all_variable_names(self):
+        # get all states, algebraics and constants
+        variable_names = list(self.simulation.results().states().keys()) + \
+                                list(self.simulation.results().algebraic().keys()) + \
+                                     list(self.data.constants().keys())
+        return variable_names
+
+    def get_results(self, obs_names):
+        """
+        gets results after a simulation
+        inputs:
+        obs_names: list of strings, stores the names of state and algebraic variables you wish to access
+        outputs:
+        results: numpy array of size (nStates + nAlgs, n_steps). This will store
+        """
+        n_obs = len(obs_names)
+        results = np.zeros((n_obs, self.n_steps + 1))
+        for JJ, obs_name in enumerate(obs_names):
+            if obs_name in self.simulation.results().states():
+                results[JJ, :] = self.simulation.results().states()[obs_name].values()[-self.n_steps - 1:]
+            elif obs_name in self.simulation.results().algebraic():
+                results[JJ, :] = self.simulation.results().algebraic()[obs_name].values()[-self.n_steps-1:]
+            else:
+                print(f'variable {obs_name} is not a model variable. model variables are')
+                print([name for name in self.simulation.results().states()])
+                print([name for name in self.simulation.results().algebraic()])
+                print('exiting')
+                exit()
+
+        return results
+
+    def get_init_param_vals(self, param_names):
+        param_init = []
+        for JJ, param_name_or_list in enumerate(param_names):
+            if isinstance(param_name_or_list, list):
+                param_init.append([])
+                for param_name in param_name_or_list:
+                    if param_name in self.data.states():
+                        param_init[JJ].append(self.data.states()[param_name])
+                    elif param_name in self.data.constants():
+                        param_init[JJ].append(self.data.constants()[param_name])
+                    else:
+                        print(f'parameter name of {param_name} doesn\'t exist in either constants or states'
+                              f'The states are:')
+                        print([name for name in self.data.states()])
+                        print('the constants are:')
+                        print([name for name in self.data.constants()])
+                        exit()
+            else:
+                param_name = param_name_or_list
+                if param_name in self.data.states():
+                    param_init.append(self.data.states()[param_name])
+                elif param_name in self.data.constants():
+                    param_init.append(self.data.constants()[param_name])
+                else:
+                    print(f'parameter name of {param_name} doesn\'t exist in either constants or states'
+                          f'The states are:')
+                    print([name for name in self.data.states()])
+                    print('the constants are:')
+                    print([name for name in self.data.constants()])
+                    exit()
+
+        return param_init
+
+    def set_param_vals(self, param_names, param_vals):
+        # ensure param_vals stores state values first, then constant values
+        for JJ, param_name_or_list in enumerate(param_names):
+            if isinstance(param_name_or_list, list):
+                for param_name in param_name_or_list:
+                    if param_name in self.data.states():
+                        self.data.states()[param_name] = param_vals[JJ]
+                    elif param_name in self.data.constants():
+                        self.data.constants()[param_name] = param_vals[JJ]
+                    else:
+                        print(f'parameter name of {param_name} doesn\'t exist in either constants or states'
+                              f'The states are:')
+                        print([name for name in self.data.states()])
+                        print('the constants are:')
+                        print([name for name in self.data.constants()])
+                        exit()
+
+            else:
+                param_name = param_name_or_list
+                if param_name in self.data.states():
+                    self.data.states()[param_name] = param_vals[JJ]
+                elif param_name in self.data.constants():
+                    self.data.constants()[param_name] = param_vals[JJ]
+                else:
+                    print(f'parameter name of {param_name} doesn\'t exist in either constants or states'
+                          f'The states are:')
+                    print([name for name in self.data.states()])
+                    print('the constants are:')
+                    print([name for name in self.data.constants()])
+                    exit()
+                    
+    # def set_initial_states(self, param_names, param_vals):
+    #     # ensure param_vals stores state values first, then constant values
+    #     for JJ, param_name_or_list in enumerate(param_names):
+    #         if isinstance(param_name_or_list, list):
+    #             for param_name in param_name_or_list:
+    #                 if param_name in self.data.states():
+    #                     self.data.states()[param_name] = param_vals[JJ]
+    #                 elif param_name in self.data.constants():
+    #                     self.data.constants()[param_name] = param_vals[JJ]
+    #                 elif param_name in self.data.algebraic():
+    #                     self.data.algebraic()[param_name] = param_vals[JJ]
+    #                 else:
+    #                     print(f'parameter name of {param_name} doesn\'t exist in either constants or states.'
+    #                           f'The states are:')
+    #                     print([name for name in self.data.states()])
+    #                     print('the constants are:')
+    #                     print([name for name in self.data.constants()])
+    #                     exit()
+
+    #         else:
+    #             param_name = param_name_or_list
+    #             if param_name in self.data.states():
+    #                 self.data.states()[param_name] = param_vals[JJ]
+    #             elif param_name in self.data.constants():
+    #                 self.data.constants()[param_name] = param_vals[JJ]
+    #             elif param_name in self.data.algebraic():
+    #                 self.data.algebraic()[param_name] = param_vals[JJ]
+    #             else:
+    #                 print(f'parameter name of {param_name} doesn\'t exist in either constants or states. '
+    #                       f'The states are:')
+    #                 print([name for name in self.data.states()])
+    #                 print('the constants are:')
+    #                 print([name for name in self.data.constants()])
+    #                 print('the algebraics are:')
+    #                 print([name for name in self.data.algebraic()])
+    #                 exit()
+
+    def modify_params_and_run_and_get_results(self, param_names, mod_factors, obs_names, absolute=False):
+
+        if absolute:
+            new_param_vals= mod_factors
+        else:
+            init_param_vals = self.get_init_param_vals(param_names)
+            new_param_vals = [a*b for a, b in zip(init_param_vals, mod_factors)]
+
+        print(new_param_vals)
+        self.set_param_vals(param_names, new_param_vals)
+
+        success = self.run()
+        print('here')
+        if success:
+            pred_obs_new = self.get_results(obs_names)
+            # reset params
+            self.reset_and_clear()
+
+        else:
+            # simulation set cost to large,
+            print('simulation failed ')
+            exit()
+
+        return pred_obs_new
+
+    def update_times(self, dt, start_time, sim_time, pre_time):
+        self.dt = dt
+        self.stop_time= pre_time + sim_time # full time of simulation
+        self.pre_steps = int(pre_time/self.dt)  # number of steps to do before storing data (used to reach steady state)
+        self.n_steps = int(sim_time/self.dt)  # number of steps for storing data
+        self.data.set_starting_point(start_time)
+        self.data.set_ending_point(start_time + self.stop_time)
+        self.tSim = np.linspace(pre_time, self.stop_time, self.n_steps + 1)  # time values for stored part of simulation
+
+    def close_simulation(self):
+        oc.close_simulation(self.simulation)
+
+
